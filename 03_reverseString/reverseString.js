afdf57a1ed95c3f756cac47a46dd0569aa8e37a5
Lecitte <111232736+Lecitte@users.noreply.github.com>
2023-02-08 23:09:01 +0300

    heh

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const array = str.split("");
+    const array1 = new Array(array.length);
+    let j = array.length - 1;
+    for(i = 0; i < array.length; i++) {
+        array1[j] = array[i];
+        j--;
+    }
+    return array1.join("");
 };
 
 // Do not edit below this line
