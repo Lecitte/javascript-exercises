678713540aaa90e7f695071c6587f9b1ba0d8b02
Lecitte <111232736+Lecitte@users.noreply.github.com>
2023-04-11 17:20:09 +0300

    hü

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const summ = arr.reduce((total, num) => {
+    return total + num;
+  }, 0);
+  return summ;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const multi = arr.reduce((multi, num) => {
+    return multi * num;
+  }, 1);
+  return multi;
+};
+
+const power = function(num1, num2) {
+  const array = [num1, num2];
+	const powe = array.reduce((pow, num) => {
+    return pow ** num;
+  });
+  return powe;
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0) return 1;
+  let start = 1;
+  for(let i = num; i > 0; i--) {
+    start = start*i;
+  }
+  return start;
 };
 
 // Do not edit below this line
